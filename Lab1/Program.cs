4b47679cea712a1b737ba502631aa3f66d6a7e53
DronBrigadir <DronBrigadir@users.noreply.github.com>
2018-09-17 20:47:45 +0300

    add functions

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,28 +8,50 @@ namespace Lab1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] coefficients = new int[3];
-            for (int i = 0; i < 3; i++) {
-                string bufCoefficient = Console.ReadLine();
-                bool checkCorrectInput = int.TryParse(bufCoefficient, out coefficients[i]);
-                while (!checkCorrectInput) {
-                    Console.WriteLine("Неправильно введен коэффициент, введите заново");
-                    bufCoefficient = Console.ReadLine();
-                    checkCorrectInput = int.TryParse(bufCoefficient, out coefficients[i]);
-                }
-            }
-            int discriminant = coefficients[1] * coefficients[1] - 4 * coefficients[0] * coefficients[2];
+        static public int Discr(List<int>coef){ ///find discriminant
+            return coef[1] * coef[1] - 4 * coef[0] * coef[2];
+        }
+        static public int Coef(){ ///enter coefficient
+            string bufCoefficient;
+            int coefficient;
+            bool checkCorrectInput = true;
+            do{
+                if (checkCorrectInput == false)
+                    Console.WriteLine("Неправильно введен коэффициент");
+                bufCoefficient = Console.ReadLine();
+                checkCorrectInput = int.TryParse(bufCoefficient, out coefficient);
+            } while (!checkCorrectInput);
+            return coefficient;
+        }
+        static public List<double> SolveEquation(List<int> coef){
+            int discriminant = Discr(coef);
+            List<double> result = new List<double>();
             if (discriminant < 0)
                 Console.WriteLine("Нет решения");
-            if (discriminant == 0)
-                Console.WriteLine("Один корень = {0}", -coefficients[1]/(2*coefficients[0]));
-            if (discriminant > 0)  {
+            if (discriminant == 0){
+                double res = -coef[1] / (2 * coef[0]);
+                result.Add(res);
+                Console.WriteLine("Один корень = {0}", res);
+            }
+            if (discriminant > 0)
+            {
                 double res1, res2;
-                res1 = (-coefficients[1] + Math.Sqrt(discriminant))/(2*coefficients[0]);
-                res2 = (-coefficients[1] - Math.Sqrt(discriminant))/(2*coefficients[0]);
-                Console.WriteLine("Первый корень = {0:F3}, второй корень = {1:F3}", res1, res2);
+                res1 = (-coef[1] + Math.Sqrt(discriminant)) / (2 * coef[0]);
+                res2 = (-coef[1] - Math.Sqrt(discriminant)) / (2 * coef[0]);
+                result.Add(res1);
+                result.Add(res2);
+                Console.WriteLine("Первый корень = {0:F3}, второй корень = {1:F3}", result[0], result[1]);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            List<int> coefficients = new List<int>();
+            for (int i = 0; i < 3; i++) {
+                coefficients[i] = Coef();
+            }
+            int discriminant = Discr(coefficients);
+            SolveEquation(coefficients);
             }
         }
     }
